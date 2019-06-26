0b80483ef93889a92ba573c043dbeed5152febb3
user <user@dbtest2.sai.msu.ru>
2019-06-26 21:32:43 +0300

    add testUrat1

---
+++ testUrat1z326.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import struct
+import os
+
+z_catalog = "."   # папка с каталогами "/home/source_cat/URAT1/v12"    
+binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'   # формат чисел struct
+#binary_file = 'z326'     # читаемый файл из папки с каталогами 
+#dump_file = 'testW.asc'   # файл для dump 
+#list_row = 'RA, spd, sigs, sigm, nst, nsu, \
+                #epoc, mmag, sigp, nsm, ref, \
+                #nit, niu, ngt, ngu, pmr, pmd, \
+                #pme, mf2, mfa, id2, jmag, hmag, \
+                #kmag, ejmag, ehmag, ekmag, iccj, \
+                #icch, icck, phqj, phqh, phqk, abm, \
+                #avm, agm, arm, aim, ebm, evm, egm, '
+                #erm, eim, ann, ano'      # кортеж столбцов таблицы
+col = ['RA', 'spd', 'sigs', 'sigm', 'nst', 'nsu', 'epoc', 'mmag', 'sigp', 'nsm', 'ref', 'nit', 'niu', 'ngt', 'ngu', 'pmr', 'pmd', 'pme', 'mf2', 'mfa', 'id2', 'jmag', 'hmag', 'kmag', 'ejmag', 'ehmag', 'ekmag', 'iccj', 'icch', 'icck', 'phqj', 'phqh', 'phqk', 'abm', 'avm', 'agm', 'arm', 'aim', 'ebm', 'evm', 'egm', 'erm', 'eim', 'ann', 'ano']
+#list_str = [10, 9, 3, 3, 2, 3, 5, 5, 3, 2, 1, 3, 3, 3, 3, 5, 5, 3, 2, 2, 10, 5, 5, 5, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 3, 3]
+
+
+
+
+# проход по файлам
+
+for root, dirs, files in os.walk(z_catalog):
+    zfiles = [f for f in files if f.startswith("z")]
+    #print("Find {} z files in {} ".format(len(zfiles), z_catalog)) 
+    for z_files in zfiles:
+        full_name_path = os.path.join(root, z_files)
+        #print(full_name_path)
+        with open(full_name_path, 'rb') as fin:
+            all_catalog = []
+            din = True
+            while din:
+                din = fin.read(80)
+                if len(din) == 80:
+                    list_row = struct.unpack(binary_unpack, din)
+                    all_catalog.append(list_row)
+                    #print(a)
+                    #to_print = ("".join("{:" + "{}".format(list_str[i]) + "} " for i in range(len(list_row)))).format(*( _ for _ in list_row))
+df = pd.DataFrame(all_catalog, columns = col)
+print(df.head())
+                    #print(to_print)
+                    #with open(dump_file, 'a') as fout:
+                        #fout.write(to_print +'\n')
+
+
+#df.to_sql(address= , user=, paswd=, database=,  )
+
+
+# выводим количество байтов записанных в файл
+#s = os.path.getsize(dump_file)
+#print (s)
