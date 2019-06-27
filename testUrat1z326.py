daff6690406b5bc7d52106bb2f20a03f4cd9feb4
Alexandra Galdina <nerenisa@gmail.com>
2019-06-27 16:11:20 +0300

    added listdir(catalog)

--- testUrat1z326.py
+++ testUrat1z326.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 import struct
 import os
 
-z_catalog = "."   # папка с каталогами "/home/source_cat/URAT1/v12"    
+z_catalog = r"C:\Users\User\Documents\urat1test"   # папка с каталогами "/home/source_cat/URAT1/v12"    
 binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'   # формат чисел struct
 #binary_file = 'z326'     # читаемый файл из папки с каталогами 
 #dump_file = 'testW.asc'   # файл для dump 
@@ -24,22 +25,24 @@ col = ['RA', 'spd', 'sigs', 'sigm', 'nst', 'nsu', 'epoc', 'mmag', 'sigp', 'nsm',
 
 
 
-# проход по файлам
+# walk through files in a directory z_catalog
 
-for root, dirs, files in os.walk(z_catalog):
-    zfiles = [f for f in files if f.startswith("z")]
+#for files in os.listdir(z_catalog):
+#for root, dirs, files in os.walk(z_catalog):
+zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z")]
+print(zfiles)
     #print("Find {} z files in {} ".format(len(zfiles), z_catalog)) 
-    for z_files in zfiles:
-        full_name_path = os.path.join(root, z_files)
+for z_files in zfiles:
+    full_name_path = os.path.join(z_files)
         #print(full_name_path)
-        with open(full_name_path, 'rb') as fin:
-            all_catalog = []
-            din = True
-            while din:
-                din = fin.read(80)
-                if len(din) == 80:
-                    list_row = struct.unpack(binary_unpack, din)
-                    all_catalog.append(list_row)
+    with open(full_name_path, 'rb') as fin:
+        all_catalog = []
+        din = True
+        while din:
+            din = fin.read(80)
+            if len(din) == 80:
+                list_row = struct.unpack(binary_unpack, din)
+                all_catalog.append(list_row)
                     #print(a)
                     #to_print = ("".join("{:" + "{}".format(list_str[i]) + "} " for i in range(len(list_row)))).format(*( _ for _ in list_row))
 df = pd.DataFrame(all_catalog, columns = col)
