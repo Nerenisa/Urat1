39773698bff6bca19ae7e4bb6be4f675825da4c2
Alexandra Galdina <nerenisa@gmail.com>
2019-06-28 15:34:28 +0300

    add connect to_sql

--- testUrat1z326.py
+++ testUrat1z326.py
@@ -7,6 +7,7 @@ import numpy as np
 import struct
 import os
 
+
 z_catalog = r"C:\Users\User\Documents\urat1test"   # folder with directories "/home/source_cat/URAT1/v12"    
 binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'   # format characters module struct (80 bytes)
 #binary_file = 'z326'     # read file from the folder with directories 
@@ -21,6 +22,9 @@ binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'   # format chara
                 #erm, eim, ann, ano'      # tuple of table columns
 col = ['RA', 'spd', 'sigs', 'sigm', 'nst', 'nsu', 'epoc', 'mmag', 'sigp', 'nsm', 'ref', 'nit', 'niu', 'ngt', 'ngu', 'pmr', 'pmd', 'pme', 'mf2', 'mfa', 'id2', 'jmag', 'hmag', 'kmag', 'ejmag', 'ehmag', 'ekmag', 'iccj', 'icch', 'icck', 'phqj', 'phqh', 'phqk', 'abm', 'avm', 'agm', 'arm', 'aim', 'ebm', 'evm', 'egm', 'erm', 'eim', 'ann', 'ano']
 #list_str = [10, 9, 3, 3, 2, 3, 5, 5, 3, 2, 1, 3, 3, 3, 3, 5, 5, 3, 2, 2, 10, 5, 5, 5, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 3, 3]
+engine = create_engine('postgresql://user:147456@158.250.29.198:5433/test2')
+
+
 
 
 
@@ -48,7 +52,7 @@ print(df)
                         #fout.write(to_print +'\n')
 
 
-#df.to_sql(address= , user=, paswd=, database=,  )
+df.to_sql('table_Urat1_test', engine)
 
 
 # print number of bytes
