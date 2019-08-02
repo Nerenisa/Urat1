1ddf50ffc83356a74a7e108d5b9368698e6d8edf
Alexandra Galdina <nerenisa@gmail.com>
2019-08-02 13:32:07 +0300

    new file pr.py

---
+++ pr.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import pandas as pd
+import os
+import re
+import numpy as np
+import struct
+import os
+
+z_catalog = r"C:\Users\User\Documents\urat1test"
+binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'
+col = ['RA', 'spd', 'sigs', 'sigm', 'nst', 'nsu', 'epoc', 'mmag', 'sigp', 'nsm', 'ref', 'nit', 'niu', 'ngt', 'ngu', 'pmr', 'pmd', 'pme', 'mf2', 'mfa', 'id2', 'jmag', 'hmag', 'kmag', 'ejmag', 'ehmag', 'ekmag', 'iccj', 'icch', 'icck', 'phqj', 'phqh', 'phqk', 'abm', 'avm', 'agm', 'arm', 'aim', 'ebm', 'evm', 'egm', 'erm', 'eim', 'ann', 'ano']
+
+
+zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z")]
+for z_files in zfiles:
+    full_name_path = os.path.join(z_catalog, z_files)
+    with open(full_name_path, 'rb') as fin:
+        din = True
+        n = 0
+        all_catalog = []   
+        idn = []      # new 
+        s = []     # new
+        while din:
+            din = fin.read(80)
+            if len(din) == 80:
+                list_row = list(struct.unpack(binary_unpack, din))
+                all_catalog.append(list_row)
+                n = n + 1
+                s.append(n)
+        for f in s:
+            print(f)
+        zn = ''.join((z_files.lstrip('z'), '-')).split() * all_catalog.__len__()  # new
+        for zin in zn:
+            idn.append(''.join((zin, str(f))))
+            #print(idn)
+            #print(e)
+
+
+        df = pd.DataFrame(all_catalog, index = idn, columns = col)
+        print(df)
+
+
+
+
+
+
+
+
+
+
+
+
--- testUrat1.py
+++ testUrat1.py
@@ -22,7 +22,6 @@ binary_unpack = 'iihhBbhhhBBhhBBhhhBBihhhhhhBBBBBBhhhhhhhhhhBB'   # format chara
                 #avm, agm, arm, aim, ebm, evm, egm, '
                 #erm, eim, ann, ano'      # tuple of table columns
 col = ['RA', 'spd', 'sigs', 'sigm', 'nst', 'nsu', 'epoc', 'mmag', 'sigp', 'nsm', 'ref', 'nit', 'niu', 'ngt', 'ngu', 'pmr', 'pmd', 'pme', 'mf2', 'mfa', 'id2', 'jmag', 'hmag', 'kmag', 'ejmag', 'ehmag', 'ekmag', 'iccj', 'icch', 'icck', 'phqj', 'phqh', 'phqk', 'abm', 'avm', 'agm', 'arm', 'aim', 'ebm', 'evm', 'egm', 'erm', 'eim', 'ann', 'ano']
-zn = list(range(326, 901))
 #list_str = [10, 9, 3, 3, 2, 3, 5, 5, 3, 2, 1, 3, 3, 3, 3, 5, 5, 3, 2, 2, 10, 5, 5, 5, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 3, 3]
 #pg_engine = create_engine('postgresql+psycopg2://user:147456@158.250.29.198:5433/test2')
 #psql = 'select * from table_Urat1_test limit 5;'
@@ -31,7 +30,7 @@ zn = list(range(326, 901))
 # walk through files in a directory z_catalog
 
 #for root, dirs, files in os.walk(z_catalog):
-zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z525")]
+zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z")]
 #print("Find {} z files in {} ".format(len(zfiles), z_catalog)) 
 all_catalog = []
 for z_files in zfiles:
@@ -44,7 +43,7 @@ for z_files in zfiles:
                 list_row = list(struct.unpack(binary_unpack, din))
                 all_catalog.append(list_row)
 #print(all_catalog)
-                #all_catalog.append(list_row)
+   
                     #to_print = ("".join("{:" + "{}".format(list_str[i]) + "} " for i in range(len(list_row)))).format(*( _ for _ in list_row))
 df = pd.DataFrame(all_catalog, columns = col)
 print(df)
